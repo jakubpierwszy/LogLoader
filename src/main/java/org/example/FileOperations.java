eba6603426d76d258b2e2c0d0e37e9209b6b9d6d
jakub <jakubledzionlg@gmail.com>
2023-11-02 13:05:42 +0100

    Dates and Times Patterns functions

--- src/main/java/org/example/FileOperations.java
+++ src/main/java/org/example/FileOperations.java
@@ -16,7 +16,7 @@ public class FileOperations {
 
             @Override
             public int compare(File f1, File f2) {
-                return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
+                return Long.valueOf(f2.lastModified()).compareTo(f1.lastModified());
             }
         });
         return files;
@@ -25,18 +25,33 @@ public class FileOperations {
     public void readFile(File file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String currentLine = reader.lines().collect(Collectors.joining());
-        System.out.println(currentLine);
-        System.out.println(currentLine.subSequence(0, 10));
+        //System.out.println(currentLine);
 
-        Pattern pattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}");
-        Matcher matcher = pattern.matcher(currentLine);
-        List<String> dates = new ArrayList<>();
+        //datePattern(currentLine);
+        //System.out.println(timePattern(currentLine).get(10));
 
-        while (matcher.find()) {
-            dates.add(matcher.group() + " " + matcher.start());
-        }
-        System.out.println(dates.get(1));
 
         reader.close();
     }
+
+    public List<String> datePattern(String logs) {
+        Pattern pattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}");
+        Matcher matcher = pattern.matcher(logs);
+        List<String> dates = new ArrayList<>();
+        while (matcher.find()) {
+            dates.add(matcher.group());
+        }
+        return dates;
+    }
+
+    public List<String> timePattern(String logs) {
+        final Pattern pattern = Pattern.compile("[0-9]{2}:[0-9]{2}:[0-9]{2}(\\.[0-9]{1,3})?");
+        final Matcher matcher = pattern.matcher(logs);
+        List<String> times = new ArrayList<>();
+        while (matcher.find()) {
+            times.add(matcher.group());
+        }
+        return times;
+    }
+
 }
